7e1999a27e3abd93ed793e07993d2ef29ad9a78f
waris <vip.waris@gmail.com>
2019-06-19 17:49:51 +0300

    DjangoProjesi2 canliya alma islemi

---
+++ accounts/forms.py
@@ -0,0 +1,80 @@
+from __future__ import unicode_literals
+from django.contrib.auth.forms import AuthenticationForm
+from django import forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit, HTML, Field
+from authtools import forms as authtoolsforms
+from django.contrib.auth import forms as authforms
+from django.urls import reverse
+
+
+class LoginForm(AuthenticationForm):
+    remember_me = forms.BooleanField(required=False, initial=False)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.fields["username"].widget.input_type = "email"  # ugly hack
+
+        self.helper.layout = Layout(
+            Field("username", placeholder=" Email", autofocus=""),
+            Field("password", placeholder=" Şifre "),
+            HTML(
+                '<a href="{}">Şifremi Unuttum ?</a>'.format(
+                    reverse("accounts:password-reset")
+                )
+            ),
+            Field("remember_me"),
+            Submit("sign_in", "Giriş Yap ", css_class="btn btn-lg btn-primary btn-block"),
+        )
+
+
+class SignupForm(authtoolsforms.UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.fields["email"].widget.input_type = "email"  # ugly hack
+
+        self.helper.layout = Layout(
+            Field("email", placeholder=" Email Gir ", autofocus=""),
+            Field("name", placeholder="Kullanıcı Adını Gir"),
+            Field("password1", placeholder="Şifreyi Gir"),
+            Field("password2", placeholder="Şifreyi Tekrar Gir"),
+            Submit("sign_up", "kayıt Ol", css_class="btn-warning"),
+        )
+
+
+class PasswordChangeForm(authforms.PasswordChangeForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+
+        self.helper.layout = Layout(
+            Field("old_password", placeholder="Enter old password", autofocus=""),
+            Field("new_password1", placeholder="Enter new password"),
+            Field("new_password2", placeholder="Enter new password (again)"),
+            Submit("pass_change", "Change Password", css_class="btn-warning"),
+        )
+
+
+class PasswordResetForm(authtoolsforms.FriendlyPasswordResetForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+
+        self.helper.layout = Layout(
+            Field("email", placeholder="Enter email", autofocus=""),
+            Submit("pass_reset", "Reset Password", css_class="btn-warning"),
+        )
+
+
+class SetPasswordForm(authforms.SetPasswordForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+
+        self.helper.layout = Layout(
+            Field("new_password1", placeholder="Enter new password", autofocus=""),
+            Field("new_password2", placeholder="Enter new password (again)"),
+            Submit("pass_change", "Change Password", css_class="btn-warning"),
+        )
